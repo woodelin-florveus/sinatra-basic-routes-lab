b02b96a1eeac6762fc7f57fb805a997408deb0fd
woodelin-florveus <woodelinflorveus@gmail.com>
2021-01-10 22:16:00 -0500

    commit tree 5700b9727d7ca3ee371bea601088c51f14778c67

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do 
+        "My name is woodelin"
+    end
+
+    get '/hometown' do
+        "My hometown is Brooklyn"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is Megaman by Lil-Wayne"
+    end
+
 end
